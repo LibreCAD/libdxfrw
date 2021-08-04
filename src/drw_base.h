7fbd7329a48a4f6bf2b2ea0487ba6ed9f59d6af4
Nyall Dawson <nyall.dawson@gmail.com>
2021-08-05 08:53:02 +1000

    Remove unnecessary assignment operator from DRW_Coord

--- src/drw_base.h
+++ src/drw_base.h
@@ -158,13 +158,9 @@ enum TransparencyCodes {
 */
 class DRW_Coord {
 public:
-    DRW_Coord():x(0), y(0),z(0) {}
+    DRW_Coord()=default;
     DRW_Coord(double ix, double iy, double iz): x(ix), y(iy),z(iz){}
 
-     DRW_Coord& operator = (const DRW_Coord& data) {
-        x = data.x;  y = data.y;  z = data.z;
-        return *this;
-    }
 /*!< convert to unitary vector */
     void unitize(){
         double dist;
@@ -177,9 +173,9 @@ public:
     }
 
 public:
-    double x;
-    double y;
-    double z;
+    double x{0};
+    double y{0};
+    double z{0};
 };
 
 
